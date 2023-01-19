bcf5d1aae3934191fe6d05366acd7e081ade5242
dleiferives <dleiferives@gmail.com>
2023-01-18 23:00:04 -0800

    first code generation

---
+++ helpers.h
@@ -0,0 +1,31 @@
+
+char* itoa(int value, int base) {
+	//puts("itoa start");
+    char * result = malloc(sizeof(char) * 20);
+    for(int i = 0; i<20; i++)
+    {
+	    result[i] = 0;
+    }
+    // check that the base if valid
+    if (base < 2 || base > 36) { *result = '\0'; return result; }
+
+    char* ptr = result, *ptr1 = result, tmp_char;
+    int tmp_value;
+
+    do {
+        tmp_value = value;
+        value /= base;
+        *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+    } while ( value );
+
+    // Apply negative sign
+    if (tmp_value < 0) *ptr++ = '-';
+    *ptr-- = '\0';
+    while(ptr1 < ptr) {
+        tmp_char = *ptr;
+        *ptr--= *ptr1;
+        *ptr1++ = tmp_char;
+    }
+    //puts("itoa end");
+    return result;
+}
